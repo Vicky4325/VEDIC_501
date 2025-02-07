c1ae6f15e9d96efb5ec50e3ee18b9915e4390730
Nalluri Vignesh <160838659+Vicky4325@users.noreply.github.com>
2025-02-07 23:46:44 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool
+  .connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.error("Error connecting to PostgreSQL", err));
+
+module.exports = pool;
